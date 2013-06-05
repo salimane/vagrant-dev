fa82030d3cb7fbd13792b8128952d520203a44de
Salimane Adjao Moustapha <me@salimane.com>
2013-06-05 16:49:53 +0800

    load sites-available confs in nginx and fix augeas crying dependencies

--- puppet/manifests/nginxsetup.pp
+++ puppet/manifests/nginxsetup.pp
@@ -4,4 +4,10 @@ class nginxsetup {
 
     class { 'nginx':
     }
+
+    file { '/etc/nginx/conf.d/include-sites.conf':
+        ensure => file,
+        content => 'include /etc/nginx/sites-available/*;',
+        notify => Class['nginx::service'],
+    }
 }
